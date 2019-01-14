051dadabb2712de9e888980c71e89031fb80f88f
= <=>
2019-01-14 14:35:47 +0100

    :construction: fetching list and showing title

--- controllers/boardController.php
+++ controllers/boardController.php
@@ -16,3 +16,31 @@ while($donnees = $res->fetch()) {
     $lists[] = $donnees;
 }
 
+$res->closeCursor();
+
+// Cas où je reçcois une variable Post de _form_list.php : je crée une liste
+if (!empty($_POST['list-title']) ) {
+    // Création d'une nouvelle liste : 
+
+    $res = $bdd->prepare("INSERT INTO lists(title) VALUES (:title)");
+
+    $res->execute([
+        "title" => $_POST['list-title']
+    ]);
+    // Change en tête de mesage en http
+    Header('Location: /splists/views/board.php' . $bdd->lastInsertId());
+}
+    // READ (1 élément) : Lecture d'une Liste.
+function getList($idList) {
+    $bdd = dbConnect('splists', 'root', '', 3308);
+
+    $request ='SELECT * FROM lists WHERE id = ' . $idList;
+
+    $response = $bdd->query($request);
+    
+    // Je m'attends à recevoir 1 seul élément, je ne fais pas de while (tant que...)
+    // Car while (tant que...) me sert à dire : "tant qu'il y a des éléments qui vont dans $liste"
+    $liste = $response->fetch();
+
+    return $liste;
+}
