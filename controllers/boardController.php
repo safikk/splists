abfb7a5716c5e280346f2711220837c9486a2cc9
= <=>
2019-01-15 09:38:12 +0100

    :sparkles: show tasks fonctionne

--- controllers/boardController.php
+++ controllers/boardController.php
@@ -1,71 +1,64 @@
 <?php
 require_once('helper.php');
-
+/**
+ * Initialisation de l'instance de base de données
+ * Instanciation de base de données
+ */
 $bdd = dbConnect('splists', 'root', '', 3308);
-
-// Response de la BDD non traitee
+/**
+ * READ (tous les éléments): Lecture de toutes les listes dès que j'accède à board.php
+ */
+// Response de la BDD non traitée
 $res = $bdd->query('SELECT * FROM lists');
-
-       // Si je veux voir la derniere erreur en BDD : $bdd->errorinfo()
-
-// J'instancie mon tableau qui contoiendra mes listes
+// Si je veux voir la dernière erreur en BDD : $bdd->errorinfo()
+// J'instancie mon tableau qui contiendra mes listes
 $lists = [];
-
-// Tant que j'ai des reponses qui vont dans $donnees (variable temporaire pour le while)
+// Tant que j'ai des réponses qui vont dans $donnees (variable temporaire pour le while)
 while($donnees = $res->fetch()) {
-
-        // On prend chaque ligne de "$donnees" qu'on met dans L'array $lists
+    // Je met le contenu de $donnees (variable temporaire) dans le tableau $lists
     $lists[] = $donnees;
 }
-
 $res->closeCursor();
-
-// Cas où je reçcois une variable Post de _form_list.php : je crée une liste
-if (!empty($_POST['list-title']) ) {
-    // Création d'une nouvelle liste : 
-
+/**
+ * CREATE : Cas où je reçois une variable POST de _form_list.php : je créée une liste
+ */
+if( !empty($_POST['list-title']) ) {
+    // Création d'une nouvelle liste :
     $res = $bdd->prepare("INSERT INTO lists(title) VALUES (:title)");
-
     $res->execute([
         "title" => $_POST['list-title']
     ]);
-    // Change en tête de mesage en http
-    Header('Location: /splists/views/board.php' . $bdd->lastInsertId());
+    Header('Location: /splists/views/board.php?list=' . $bdd->lastInsertId());
 }
-   
-// READ (1 élément) : Lecture d'une Liste.
+/**
+ * READ (1 élément) : Lecture d'une liste
+ */
 function getList($idList) {
     $bdd = dbConnect('splists', 'root', '', 3308);
-
-    $request ='SELECT * FROM lists WHERE id = ' . $idList;
-
+    $request = 'SELECT *
+                FROM lists
+                WHERE id = ' . $idList;
     $response = $bdd->query($request);
-    
     // Je m'attends à recevoir 1 seul élément, je ne fais pas de while (tant que...)
-    // Car while (tant que...) me sert à dire : "tant qu'il y a des éléments qui vont dans $liste"
+    // car while (tant que...) me sert à dire : "tant qu'il y a des éléments qui vont dans $liste"
+    // or, je n'en ai qu'1 seul qui va dans liste !
+    // (voir "select * from lists" ci-dessus pour un cas avec while)
     $liste = $response->fetch();
-
     return $liste;
 }
-
-
-function getTasks($idTasks) {
+function getTasks($idList) {
     $bdd = dbConnect('splists', 'root', '', 3308);
-
-    $request ='SELECT * FROM tasks WHERE id_list = ' . $idTasks;
-
+    $request = 'SELECT *
+                FROM tasks
+                WHERE id_list = ' . $idList;
     $response = $bdd->query($request);
-    
-    // J'initialise un tableau de tache vide
+    // J'initialise un tableau de tâches vide
     $tasks = [];
-
     // Tant que j'ai des données reçues...
     while ($donnees = $response->fetch()) {
-
-        // j'ajoute (en mettant des crochets au nom de l'array) dans le tableau $task
+        // ... j'ajoute (en mettant des crochets au nom de l'array) dans le tableau $task
         // ce que j'ai reçu dans $donnees
         $tasks[] = $donnees;
     }
-
     return $tasks;
-}
+} 
--- views/board.php
+++ views/board.php
@@ -3,7 +3,43 @@
     $_SESSION['page'] = 'board';
 ?>
 
-@@ -51,76 +52,33 @@
+<?php include('../partials/_header.php'); ?>
+<?php include('../partials/_form_list.php'); ?>
+<!-- CONTENU DE index.php, board.php, login.php, signup.php -->
+<div class="container-fluid mt-2">
+    <div class="row">
+        <!-- SIDEBAR : Liste de listes -->
+        <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+            <div class="sidebar-sticky">
+                <ul class="nav flex-column">
+                    <?php
+                    
+                    foreach($lists as $l) {
+                        ?>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="/splists/views/board.php?list=<?= $l['id']; ?>">
+                                    <span data-feather="home"></span>
+                                    <?= $l['title']; ?> <span class="sr-only">(current)</span>
+                                </a>
+                            </li>
+                    <?php
+                    }
+                    ?>
+                    <li><hr></li>
+                    <li class="ml-2">
+                        <button class="btn btn-sm btn-primary"
+                                type="button"
+                                data-toggle="modal"
+                                data-target="#formList">Nouvelle liste</button>
+                    </li>
+                    
+                </ul>
+            </div>
+        </nav>
+        <!-- MAIN CONTENT : Tickets appartenants à une liste -->
+        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+        <?php
+            if ( isset($_GET['list']) && intval($_GET['list'] > 0) ){
                 // Cas où j'ai un ID de liste
                 $liste = getList($_GET['list']);
                 $tasks = getTasks($_GET['list']);
@@ -98,3 +134,14 @@
                         </div>
                 <?php
             }
+            else {
+                // Cas où je n'ai pas d'ID de liste
+                ?>
+                <h1>Bienvenue sur votre board.</h1>
+                <?php
+            }
+        ?>
+        </main>
+    </div>
+</div>
+<?php include('../partials/_footer.php'); ?>
